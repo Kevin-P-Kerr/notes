d76fc150fdbc55bece845d81004e52c8037a5b4d
Kevin <kkerr87@gmail.com>
2018-08-04 15:25:33 -0400

    interesting

--- src/main/java/com/notes/Notes.java
+++ src/main/java/com/notes/Notes.java
@@ -1,5 +1,6 @@
 package com.notes;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionLagrangeForm;
@@ -13,6 +14,9 @@ public class Notes {
 		long  i =2;
 		while (l%i != 0) {
 			i++;
+		}
+		if (i==l) {
+			return 1;
 		}
 		return i;
 	}
@@ -29,14 +33,12 @@ public class Notes {
 	}
 	public static void main(String[] args) {
 		List<Tuple<Long,Long>> l = Lists.newArrayList();
-		long i = 41;
-		int n = 0;
-		while (n<4) {
-			long f = getFactor(i);
-			l.add(new Tuple<Long, Long>(i,f));
-			n++;
-			i+=41;
+		List<Long> xElems = Lists.newArrayList(9L,11L,21L,33L,47L,55L,67L,71L,85L,91L,105L);
+		for (Long xelm:xElems) {
+			long f = getFactor(xelm);
+			l.add(new Tuple<Long, Long>(xelm,f));
 		}
+		
 		List<Double> x = Lists.newArrayList();
 		List<Double> y = Lists.newArrayList();
 		for (Tuple<Long, Long> t:l) {
@@ -47,13 +49,23 @@ public class Notes {
 		}
 		PolynomialFunctionLagrangeForm poly = new PolynomialFunctionLagrangeForm(Doubles.toArray(x), Doubles.toArray(y));
 		String ps = poly.toString();
-		for (int z =4,ii=105;z<ii;z++) {
+		String polystr = "";
+		for (int z =3,ii=105;z<=ii;z+=2) {
 			double v = poly.value(z*1.0);
 			long fv = Math.round(v);
 			long fact = getFactor(z);
-			System.out.println(String.format("%d : %d", fv,fact));
-			
+			System.out.println(String.format("%d: %d : %d",z, fv,fact));			
 		}
+		List<Double> co = Doubles.asList(poly.getCoefficients());
+		
+		int pow = co.size()-1;
+		for (Double c: co) {
+			polystr += String.format("%fx^%d ", c,pow);
+			pow--;
+		}
+		System.out.println(polystr);
+		
+
 		
 	}
 
