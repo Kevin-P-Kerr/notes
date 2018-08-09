dbecd4177f4a6ba979da05efd5dce8e229b96885
Kevin Kerr <kkerr@triplelift.com>
2018-08-09 12:58:26 -0400

    to str

--- ocaml/arith.ml
+++ ocaml/arith.ml
@@ -9,6 +9,11 @@ type quantpart = FORALL | THEREIS
 type quant = Q of quantpart * variable | QR of quant * quant;;
 type formula = FO of arithexpr * eq * arithexpr | FL of formula * lc * formula | QF of quant * formula;;
 
+let formal_number_tostr x:formal_number = x;;
+let rec tostr x:arithexpr =
+    match x with 
+    |    F(f) -> formal_number_tostr x
+    | A(f,o,a) -> formal_
 let z v = FO (A((V v), PLUS,  F(C("0"))),EQUALS ,F(V(v)));;
 let r va vb = FO (A(V(va), PLUS, F(V(vb))), EQUALS, A(V(vb), PLUS, F(V(va))));;
 
