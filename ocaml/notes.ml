62cf60c1a9163bb56f19a53a7da179365d28b442
Kevin <kkerr87@gmail.com>
2018-08-09 08:56:25 -0400

    more notes

--- ocaml/notes.ml
+++ ocaml/notes.ml
@@ -4,6 +4,7 @@ type formal_number = C of constant | V of variable;;
 type op = PLUS | MINUS | TIMES | DIVIDE;;
 type eq = EQUALS;;
 type arithexpr = F of formal_number | A of formal_number * op * arithexpr;;
-type formula = arithexpr * eq * arithexpr;;
+type formula = FO of arithexpr * eq * arithexpr;;
 
-let z v = formula A((V v), PLUS,  F(C("0"))),EQUALS ,F(V(v));;
+let z v = FO (A((V v), PLUS,  F(C("0"))),EQUALS ,F(V(v)));;
+let r va vb = FO (A(V(va), PLUS, F(V(vb))), EQUALS, A(V(vb), PLUS, F(V(va))));;
