f1873fb95c6478e5f5dba42879dee1245df2293a
Kevin Kerr <kkerr@triplelift.com>
2018-08-09 11:27:24 -0400

    more logical types

--- ocaml/notes.ml
+++ ocaml/notes.ml
@@ -2,9 +2,13 @@ type constant = string;;
 type variable = string;;
 type formal_number = C of constant | V of variable;;
 type op = PLUS | MINUS | TIMES | DIVIDE;;
-type eq = EQUALS;;
+type eq = EQUALS | GT | LT;;
+type lc = IMP | DOUBLE_IMP | DISJ | CONJ;;
 type arithexpr = F of formal_number | A of formal_number * op * arithexpr;;
-type formula = FO of arithexpr * eq * arithexpr;;
+type quantpart = FORALL | THEREIS
+type quant = Q of quantpart * variable | QR of quant * quant;;
+type formula = FO of arithexpr * eq * arithexpr | FL of formula * lc * formula | QF of quant * formula;;
 
 let z v = FO (A((V v), PLUS,  F(C("0"))),EQUALS ,F(V(v)));;
 let r va vb = FO (A(V(va), PLUS, F(V(vb))), EQUALS, A(V(vb), PLUS, F(V(va))));;
+
