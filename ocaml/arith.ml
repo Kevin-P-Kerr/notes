192b7087a44b8aa3ee00c0cacba6b6083528acf9
Kevin Kerr <kkerr@triplelift.com>
2018-08-09 18:07:24 -0400

    more printing

--- ocaml/arith.ml
+++ ocaml/arith.ml
@@ -1,5 +1,5 @@
-type constant = string;;
-type variable = string;;
+type constant = CN of string;;
+type variable = VAR of string;;
 type formal_number = C of constant | V of variable;;
 type op = PLUS | MINUS | TIMES | DIVIDE;;
 type eq = EQUALS | GT | LT;;
@@ -11,15 +11,28 @@ type formula = FO of arithexpr * eq * arithexpr | FL of formula * lc * formula |
 
 let fntostr x =
     match x with
-        | C(y) -> y
-        | V(v) -> v
+        | C(CN y) -> y
+        | V(VAR v) -> v
+
+let opstr x =
+    match x with
+        | PLUS -> "+"
+        | MINUS -> "-"
+        | TIMES -> "*"
+        | DIVIDE -> "/"
 
 let rec tostr x =
     match x with 
     | F(f) -> fntostr f
-    | A(f,o,a) -> (fntostr f)^"+"^tostr a
+    | A(f,o,a) -> (fntostr f)^(opstr o)^tostr a
+
+let rec formulastr x =
+    match x with
+        | FO (a,e,b) -> (tostr a)^"="^(tostr b)
+        | FL (f,l,g) -> (formulastr f)^"->"^(formulastr g)
+        | QF (q,f) -> "Ax"^(formulastr f)
 
 let g x = match x with | FO(a,e,b) -> a;;
-let z v = FO (A((V v), PLUS,  F(C("0"))),EQUALS ,F(V(v)));;
-let r va vb = FO (A(V(va), PLUS, F(V(vb))), EQUALS, A(V(vb), PLUS, F(V(va))));;
+let z v = FO (A((V v), PLUS,  F(C(CN("0")))),EQUALS ,F(V(v)));;
+let r va vb = FO (A(va, PLUS, F(vb)), EQUALS, A(vb, PLUS, F(va)));;
 
