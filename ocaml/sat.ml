982659a0aff2b80df392117dc300be1c781e708b
Kevin <kkerr87@gmail.com>
2018-08-11 18:15:33 -0400

    crazy types

--- ocaml/sat.ml
+++ ocaml/sat.ml
@@ -8,17 +8,26 @@ type negate = NEGATE;;
 type atomProp = AP of atom | NAG of (negate*atom)
 type disj = D of atomProp | DJ of (atomProp*disj)
 type cnf = C of disj | CF of (disj*cnf)
+type lexrule = LR of (Str.regexp * token);;
+type parsestate = PS of (tokenInfo list * string);;
 
-let varMatch = Str.regexp "^[A-Za-z]+";;
-let asterMatch = Str.regexp "\\*";;
-let plusMatch = Str.regexp "\\+";;
-let negMatch = Str.regexp "~";;
+let varMatch = LR(Str.regexp "^[A-Za-z]+",VAR);;
+let asterMatch = LR(Str.regexp "^\\*",ASTER);;
+let plusMatch = LR(Str.regexp "^\\+",PLUS);;
+let negMatch = LR(Str.regexp "^~",NEG);;
+let reglist = [varMatch;asterMatch;plusMatch;negMatch];;
 
 let ismatch r s =
   Str.string_match r s 0;;
 
-let rec parse x y  = 
-  if String.length y == 0 then x else 
-    if ismatch varMatch y then parse (T(VAR,"a")::x) "a" else x;;
+let rec n x y z=
+  let LR(r,t) = List.nth(reglist z) in
+  if ismatch r y then PS(T(t,Str.matched_string),List.hd(Str.split(r,y))) else n x y (z+1);;
+   
+
+let rec parse n =
+  match x with 
+   | PS(x, y) ->
+    if String.length y == 0 then x else parse n x y 0;;
 
 
