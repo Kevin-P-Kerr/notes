76e845e7203d3bc48ac6a586f7730e50ecf1f5be
Kevin <kkerr87@gmail.com>
2018-08-11 15:27:37 -0400

    parsing

--- ocaml/sat.ml
+++ ocaml/sat.ml
@@ -1,5 +1,20 @@
+open Str;;
+
+type token = ASTER | PLUS | VAR | NEG;;
+type tokenInfo = (T of token * S of string);;
 type atom = A of string;;
 type negate = NEGATE;;
 type atomProp = AP of atom | NAG of (negate*atom)
 type disj = D of atomProp | DJ of (atomProp*disj)
-type cnf = C of disj | CF (disj*cnf)
+type cnf = C of disj | CF of (disj*cnf)
+
+let varMatch = Str.regexp "^[A-Za-z]+";;
+let asterMatch = Str.regexp "\\*";;
+let plusMatch = Str.regexp "\\+";;
+let negMatch = Str.regexp "~";;
+
+let rec parse x y = 
+  if String.length y == 0 then x else 
+    if (
+
+
