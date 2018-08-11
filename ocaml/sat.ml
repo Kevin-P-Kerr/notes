28678fd369f55ac0e8244a4ff6d6c3af9c5edcd1
Kevin <kkerr87@gmail.com>
2018-08-11 15:04:21 -0400

    start a sat solver

---
+++ ocaml/sat.ml
@@ -0,0 +1,5 @@
+type atom = A of string;;
+type negate = NEGATE;;
+type atomProp = AP of atom | NAG of (negate*atom)
+type disj = D of atomProp | DJ of (atomProp*disj)
+type cnf = C of disj | CF (disj*cnf)
