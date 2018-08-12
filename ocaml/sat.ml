2d0fb8135797f87be5f2b18d68d4984796c77198
Kevin <kkerr87@gmail.com>
2018-08-11 22:07:50 -0400

    debuggin

--- ocaml/sat.ml
+++ ocaml/sat.ml
@@ -17,17 +17,25 @@ let plusMatch = LR(Str.regexp "^\\+",PLUS);;
 let negMatch = LR(Str.regexp "^~",NEG);;
 let reglist = [varMatch;asterMatch;plusMatch;negMatch];;
 
-let ismatch r s =
-  Str.string_match r s 0;;
+let debug x y
+  let z = print_string z in
+  y;;
 
-let rec n x y z=
-  let LR(r,t) = List.nth(reglist z) in
-  if ismatch r y then PS(T(t,Str.matched_string),List.hd(Str.split(r,y))) else n x y (z+1);;
+let ismatch r s =
+  let b = Str.string_match r s 0 in
+  if b let y = print_string "yes" in b else b;;
+
+let rec parsework x y z=
+  let s = print_string (y^"\n") in
+  let LR(r,t) = List.nth reglist z in
+  if ismatch r y then PS(T(t,Str.matched_string y)::x,List.hd(Str.split r y)) else parsework x y (z+1);;
    
 
 let rec parse n =
-  match x with 
+  match n with 
    | PS(x, y) ->
-    if String.length y == 0 then x else parse n x y 0;;
+    if String.length y == 0 then x else parse (parsework x y 0);;
 
+let test = PS([],"a+b*d");;
 
+let z = parse test;; 
