2804b73af849186db6e2176396929aa38f55241a
Kevin Kerr <kkerr@triplelift.com>
2018-08-20 11:14:38 -0400

    move some stuff around

--- ocaml/sat.ml
+++ ocaml/sat.ml
@@ -124,7 +124,11 @@ let rec tocnfstr cnf =
   | C(d) -> todisjstr d
   | CF(d,c) -> (todisjstr d)^"*"^(tocnfstr c);;
 
-
+let rec contradictsany ap ldj =
+  match ldj with
+  | CJ(app) -> (debug (toatomstr ap) (contradicts ap app))
+  | MCJ(app,more) ->
+      if contradicts ap app then true else contradictsany ap more;;
 
 let getraw a =
   match a with 
@@ -171,52 +175,6 @@ let rec concatdnf d1 d2 =
       let d3 = concatdnf d d2 in 
       DF(s,d3);;
 
-let rec echocnf2dnf d = 
-  match d with 
-  | C(D(d)) -> J(CONJ(CJ(d)))
-  | C(DJ(a,b)) -> DF(CONJ(CJ(a)), echocnf2dnf (C(b)));;
-
-let rec contradictsany ap ldj =
-  match ldj with
-  | CJ(app) -> (debug (toatomstr ap) (contradicts ap app))
-  | MCJ(app,more) ->
-      if contradicts ap app then true else contradictsany ap more;;
-
-let distributetoconj d cj =
-  match cj with
-  | Cnt(c) -> cj
-  | CONJ(c) -> 
-      match c with
-      | CJ(ap) ->
-          if debug(toatomstr d^" "^(toatomstr ap)) (contradicts d ap) then Cnt(CONTRARY) else CONJ(MCJ(d,c))
-      | MCJ(ap,more) -> 
-          if (contradicts d ap) || (contradictsany d more) then Cnt(CONTRARY) else CONJ(MCJ(d,c))
-;;
-
-let rec distributeover d p =
-  match p with 
-  | J(cj) -> J(distributetoconj d cj)
-  | DF(cj,df) -> 
-      let nc = distributetoconj d cj in
-      let rest = distributeover d df in
-      DF(nc,rest);;
-
-let rec cnf2dnf c = 
-  match c with 
-  | C(d) -> echocnf2dnf c
-  | CF(d,e) ->
-      let p = cnf2dnf e in
-      match d with 
-      | D(ap) -> (distributeover ap p)
-      | DJ(ap,dj) ->
-          let rec helper dis =
-            match dis with
-            | D(at) -> distributeover at p
-            | DJ(aap,ddj) -> 
-                concatdnf (distributeover aap p) (helper ddj)
-          in 
-          let z = helper dj in
-          concatdnf (distributeover ap p) z;;
 
 let rec print_conj cj = 
   match cj with
@@ -232,20 +190,6 @@ let rec print_dnf d =
   match d with |
   J(s) -> print_satconj s
   | DF(s,dd) -> (print_satconj s)^"+"^(print_dnf dd);;
-
-let rec checksat x =
-    match x with
-    | J(Cnt(s)) -> false
-    | J(CONJ(s)) -> true
-    | DF(s,more) ->
-    begin
-    match s with
-    | Cnt(n) -> checksat more
-    | CONJ(n) -> true
-    end
-
-let dosat_all x =
-    checksat (cnf2dnf x)
 
 let getAtom x =
     match x with
