a89a441caec03f10ac1d0f1da5fdf6f0fd25205d
Kevin Kerr <kkerr@triplelift.com>
2018-08-31 10:39:54 -0400

    this is wrong

--- ocaml/unit.ml
+++ ocaml/unit.ml
@@ -16,14 +16,6 @@ let rec doSatRec c l =
     match c with 
     | EMPTY -> L(l)
     | CN(cnf) ->
-        let e = elim cnf l in
-        let b = List.nth e 2 in
-        if not b then 
-            let y = elimAtom cnf (negate a) in
-            let bo = List.nth y 2 in
-            if not bo then FAIL else doSatRec (List.nth y 2) (List.nth y 0)::l
-        else
-            doSatRec (List.nth e 2) (List.nth e 0)::l;;
 
 let doSat c = 
     doSatRec c [];;
