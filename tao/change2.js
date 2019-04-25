08b9275611e4cb5bf10909070cc2c6a09dfb0a63
Kevin <kkerr87@gmail.com>
2019-04-24 20:49:01 -0400

    doit

--- tao/change2.js
+++ tao/change2.js
@@ -3,10 +3,10 @@ var findMinForm = function (n,a) {
     var tail = {TAIL:true};
     var node = {};
     var b = a[a.length-1]; // assume a[n+1] < a[n]
-    if (b != 1 || b > n || b%n != 0) {
+    if (b != 1 && (b > n || b%n != 0)) {
       throw new Error();
     }
-    node.COEF = b%n;
+    node.COEF = n/b;
     node.BASE = b;
     node.RIGHT = tail;
     tail.LEFT = node;
@@ -21,28 +21,55 @@ var findMinForm = function (n,a) {
       head.RIGHT = node;
       head = node;
     }
+    head.RIGHT = tail.LEFT;
+    tail.LEFT.LEFT = head;
     return tail;
 };
 
+var copy = function (p) {
+  p = p.RIGHT;
+  var r = {TAIL:true};
+  var h = r;
+  var node;
+  while (!p.TAIL) {
+    node = {};
+    node.COEF = p.COEF;
+    node.BASE = p.BASE;
+    node.LEFT = h
+    h.RIGHT = node;
+    h = node;
+    p = p.RIGHT;
+  }
+  h.RIGHT = r;
+  r.LEFT = h;
+  return r;
+};
+
 var print = function (p) {
-    p = p.LINK;
+    p = p.RIGHT;
     var str = "";
     while (!p.TAIL) {
         str += p.COEF+"("+p.BASE+") + ";
-        p = p.LINK;
+        p = p.RIGHT;
     }
-    str += '0';
     console.log(str);
 };
 
 var a = [25,10,5,1];
 
-var makeChangeCombos = function (n) {
-    var maxForm = findMaxForm(n,a);
-    return findCombos(maxForm);
+var findCombos = function (mf) {
+  var r = [copy(mf)];
+  return r;
+
 };
 
-var z = makeChangeCombos(30);
+var makeChangeCombos = function (n,a) {
+  var minForm = findMinForm(n,a);
+  print(minForm);
+  return findCombos(minForm);
+};
+
+var z = makeChangeCombos(30,a);
 z.forEach(function (l) { print(l); });
 
 
