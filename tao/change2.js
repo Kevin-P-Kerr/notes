0836c8db80ff156f756296ba012b6488b2cb3147
Kevin <kkerr87@gmail.com>
2019-04-24 20:30:32 -0400

    we can make this much simpler

--- tao/change2.js
+++ tao/change2.js
@@ -1,91 +1,28 @@
-var findMaxForm = function (n,a) {
-    // assumes for a[i] : a[i] > a[i+1]
-    var i = 0;
-    var ii = a.length;
-    var v;
-    var q,r;
-    var qr;
+
+var findMinForm = function (n,a) {
     var tail = {TAIL:true};
-    var current = tail;
-    var node;
+    var node = {};
+    var b = a[a.length-1]; // assume a[n+1] < a[n]
+    if (b != 1 || b > n || b%n != 0) {
+      throw new Error();
+    }
+    node.COEF = b%n;
+    node.BASE = b;
+    node.RIGHT = tail;
+    tail.LEFT = node;
+    var i = 0;
+    var ii = a.length-1;
+    var head = tail;
     for (;i<ii;i++) {
-        v = a[i];
-        if (v > n) {
-            continue;
-        }
-        qr = findQR(n,v);
-        q = qr[0];
-        r = qr[1];
-        node = {};
-        node.COEF = q;
-        node.BASE = v;
-        n = r;
-        current.LINK = node;
-        current = node;
+      node = {};
+      node.BASE = a[i];
+      node.COEF = 0;
+      node.LEFT = head;
+      head.RIGHT = node;
+      head = node;
     }
-    current.LINK = tail;
     return tail;
 };
-
-var findQR = function (x,d) {
-    var q = Math.floor(x/d);
-    var r = x-(d*q);
-    return [q,r];
-};
-
-var sum = function (a,b) {
-    var r = {TAIL:true};
-    var tail = r;
-    a = a.LINK;
-    b = b.LINK;
-    while (!a.TAIL || !b.TAIL) {
-        var node = {};
-        if (a.TAIL) {
-            node.BASE = b.BASE;
-            node.COEF = b.COEF;
-            b = b.LINK;
-        }
-        else if (b.TAIL) {
-            node.BASE = a.BASE;
-            node.COEF = a.COEF;
-            a = a.LINK;
-        }
-        else if (a.BASE > b.BASE) {
-            node.BASE = a.BASE;
-            node.COEF = a.COEF;
-            a = a.LINK;
-        }
-        else if (a.BASE == b.BASE) {
-            node.BASE = a.BASE;
-            node.COEF = a.COEF + b.COEF;
-            a = a.LINK;
-            b = b.LINK;
-        }
-        else {
-            node.BASE = b.BASE;
-            node.COEF = b.COEF;
-            b = b.LINK;
-        }
-        r.LINK = node;
-        r = node;
-    }
-    r.LINK = tail;
-    return tail;
-};
-
-var toNum = function(p) {
-    p = p.LINK;
-    var r = 0;
-    while (!p.TAIL) {
-        r += (p.COEF * p.BASE);
-    }
-    return r;
-};
-
-var rebalance = function (p,a) {
-    return findMaxForm(toNum(p),a);
-};
-
 
 var print = function (p) {
     p = p.LINK;
@@ -99,44 +36,6 @@ var print = function (p) {
 };
 
 var a = [25,10,5,1];
-// stash a copy of these as we will need them alot
-var maxDenomForms = (function () {
-    var r = {};
-    var i = 0;
-    var ii = a.length;
-    var denom;
-    for (;i<ii;i++) {
-        r[a[i]] = findMaxForm(a[i], i == a.length-1? [1] : a.slice(i+1));
-    }    
-    return r;
-})();
-
-var makeNegative = function (b) {
-    var tail = {TAIL:true};
-    var r = {COEF:-1,BASE:b,LINK:tail};
-    tail.LINK = r;
-    return tail;
-};
-
-var findCombos = function (m) {
-    var r = [m];
-    var tail = m;
-    m = m.LINK;
-    var current = m;
-    while (!m.TAIL) {
-        var c = m.COEF;
-        var b = m.BASE;
-        while (c > 0) {
-            c--;
-            var dummy = makeNegative(b);
-            var n = sum(sum(dummy,tail),maxDenomForms[b]);
-            r.push(n);
-            current = n;
-        }
-        m = m.LINK;
-    }
-    return r;
-};
 
 var makeChangeCombos = function (n) {
     var maxForm = findMaxForm(n,a);
